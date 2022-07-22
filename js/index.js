74f4be6a1bb9ba4e0cce59d53b57828b72e25620
Maíra Araújo <74052943+Mairoviskiss@users.noreply.github.com>
2022-07-22 15:48:30 +0100

    starting to add js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+const numbers = document.querySelectorAll('.operand');
+const operators = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const PreviousOperand_text = document.querySelector('.previousOperation');
+const CurrentOperand_text = document.querySelector('currentOperation');
+
+class calculator {
+    constructor(PreviousOperand_text, CurrentOperand_text) {
+        this.PreviousOperand_text = PreviousOperand_text;
+        this.CurrentOperand_text = CurrentOperand_text;
+        this.clear();
+    }
+}
+
+clear() {
+    this.CurrentOperand_text = '';
+    this.PreviousOperand_text = '';
+    this.operation = undefined;
+
+}
+
+delete (){
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
 function add() {
 
 }
@@ -14,3 +52,8 @@ function divide() {
 
 }
 
+updateDisplay() {
+
+}
+
+const calculator = new Calculator (PreviousOperand_text, CurrentOperand_text);
